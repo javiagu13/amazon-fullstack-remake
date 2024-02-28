6abac65dab1ca6eb2f41c6848e0e0a1ba692cdf5
Javi Aguirre <javiagu13@gmail.com>
2024-02-28 13:05:29 +0900

    Added stateprovider and reducer to be able to store items in basket

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import "./Product.css"
+import { useStateValue } from './StateProvider';
 
 function Product({id, title, image, price, rating}) {
+
+    const [{basket}, dispatch] = useStateValue();
+
+    console.log("this is the basket >>> ", basket)
+    const addToBasket = () => {
+        // dispatch the item into the data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item:{
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            }
+        })
+    }
+
   return (
     <div className='product'>
         <div className='product__info'>
@@ -15,7 +34,7 @@ function Product({id, title, image, price, rating}) {
             </div>
         </div>
         <img className='product__image' src={image} alt=''></img>
-        <button className='product__button'>Add to basket</button>
+        <button onClick={addToBasket} className='product__button'>Add to basket</button>
     
     </div>
   )
